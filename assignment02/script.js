02fc8b67e1b14479481705677228413493e3fbe7
briannaordaz <briannaordaz5@gmail.com>
2024-10-27 19:05:40 -0700

    Wrote code to output the result of the comparison 10 * 10 is greater than 50 AND 5 * 5 is less than 50

--- assignment02/script.js
+++ assignment02/script.js
@@ -32,10 +32,11 @@ console.log(10 > 5);
 
 // 7. Write code to output the result of the comparison:
 // "Apples" is NOT equal to "Oranges"
-console.log ("Apples" !== "Oranges")
+console.log ("Apples" !== "Oranges");
 
 // 8. Write code to output the result of the comparison:
 // 10 * 10 is greater than 50 AND 5 * 5 is less than 50
+console.log (10 * 10 > 50 && 5 * 5 < 50);
 
 // 9. Write code to output the result of the comparison:
 // 1 + 2 is greater than 5 OR 1 * 10 is greater than 5
