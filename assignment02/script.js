3b3a224baa4009b528268c80c56497cfc49d2ca8
briannaordaz <briannaordaz5@gmail.com>
2024-10-27 19:12:32 -0700

    wrote the code that demonstrates an automatic type conversion

--- assignment02/script.js
+++ assignment02/script.js
@@ -43,3 +43,5 @@ console.log (10 * 10 > 50 && 5 * 5 < 50);
 console.log (1 + 2 > 5 || 1 * 10 > 5);
 
 // 10. Write code that demonstrates an automatic type conversion as shown in Chapter 01.
+let code = 10 + " Bananas";
+console.log(code);
